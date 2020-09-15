9049b8ca03267dacaaa14d8cb4c328aec49ea4e5
chibi20 <lea-aurore.pajzler@polymtl.ca>
2020-09-15 23:25:49 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 def get_number_of_char(string: str, char: str) -> int:
     occurence = 0
     for i in len(string):
-        if char == string[i]:
+        if string[i] == char:
             occurence+=1
     return occurence
 
