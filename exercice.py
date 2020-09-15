ccd5422a53cf4c1d1bc58e5af90bb8396164ff49
chibi20 <lea-aurore.pajzler@polymtl.ca>
2020-09-15 22:50:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -7,10 +7,8 @@ def is_even_len(string: str) -> bool:
     if b%2 !=0
         return True
     else    
-    return false
+    return False
     
-
-
 def remove_third_char(string: str) -> str:
     pass
 
